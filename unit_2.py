d375d74618e7e7f52fb0b1f466f795ce10b742bb
Tanathip64070045 <64070045@it.kmitl.ac.th>
2023-05-04 21:45:32 +0700

    hello

--- unit_2.py
+++ unit_2.py
@@ -1,8 +1,19 @@
 import csv
+
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+    
 with open('dataset.csv', 'r') as file:
     # create a reader object
     reader = csv.reader(file)
     # iterate over each row in the CSV file
     for row in reader:
         # do something with the data
-        print(row)
+        if isfloat(row[4]):
+            print(row[4])
+
+
