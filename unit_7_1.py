42bbf5391e04078eb33b4158c2ea58c6f8887888
Tanathip64070045 <64070045@it.kmitl.ac.th>
2023-05-06 17:48:59 +0700

    update

--- unit_7_1.py
+++ unit_7_1.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-
 # โหลดไฟล์ csv ของ Dataset Dollar Exchange Rates
 df = pd.read_csv('dataset.csv')
 df.set_index('Date', inplace=True)
@@ -11,24 +10,17 @@ df = df.loc[(df.index >= start_date) & (df.index <= end_date)]
 df.dropna(inplace=True)
 # คำนวณค่าเฉลี่ยของอัตราแลกเปลี่ยนสกุลเงิน USD ต่อหน่วยของแต่ละประเทศ
 avg_rates = 1.0
-
 # คำนวณค่าเฉลี่ยของอัตราแลกเปลี่ยนสกุลเงิน USD ทั้งหมด
 global_avg_rate = np.mean(df[['JPY=X']])
-
 # คำนวณค่า Z-score ของค่าเฉลี่ยของแต่ละประเทศ
 z_scores = (avg_rates - global_avg_rate) / np.std(df[['JPY=X']])
-
 # ตั้งช่วงความเชื่อมั่นของค่าเฉลี่ยดังกล่าวที่รวมถึงค่า Z-score ในระดับนัยสำคัญ 0.05
 confidence_interval = [global_avg_rate + np.percentile(df[['JPY=X']], 2.5), global_avg_rate + np.percentile(df[['JPY=X']], 97.5)]
 z_score_threshold = 1.96
 confident_countries = z_scores[z_scores.abs() > z_score_threshold]
-
 # แสดงผลลัพธ์
-print('คำนวณหาค่าเฉลี่ยของอัตราแลกเปลี่ยนสกุลเงิน USD ต่อหน่วยของแต่ละประเทศในช่วงวันที่ 1 มกราคม 2010 ถึง 31 ธันวาคม 2020 และตั้งช่วงความเชื่อมั่นของค่าเฉลี่ยดังกล่าวที่รวมถึงค่า Z-score ในระดับนัยสำคัญ 0.05')
 print("Average exchange rates of each country:", avg_rates)
 print("Global average exchange rate:", global_avg_rate)
 print("Z-scores of each country:", z_scores)
 print("Confidence interval:", confidence_interval)
 print("Countries with Z-scores outside the confidence interval:", confident_countries)
-
-
--- unit_7_2.py
+++ unit_7_2.py
@@ -2,17 +2,14 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import t
-
 # อ่านข้อมูลจากไฟล์ CSV ของ Dataset Dollar Exchange Rates
 df = pd.read_csv("dataset.csv")
-
 # สุ่มเลือกข้อมูลอัตราแลกเปลี่ยนสกุลเงินระหว่างประเทศเอเชียและยุโรป
 asia_rates = df[['PHP=X']]
 europe_rates = df[['EUR=X']]
 df.dropna(inplace=True)
 # คำนวณค่าผลต่างระหว่างค่าเฉลี่ยของสองกลุ่ม
 mean_diff = np.mean(asia_rates) - np.mean(europe_rates)
-
 # คำนวณค่า t-score และค่า p-value โดยใช้ t-distribution
 n1 = len(asia_rates)
 n2 = len(europe_rates)
@@ -28,11 +25,7 @@ t_critical = t.ppf(1 - alpha/2, n1+n2-2)
 margin_of_error = t_critical * se
 confidence_interval = (mean_diff - margin_of_error, mean_diff + margin_of_error)
 
-print('ในการศึกษาความแตกต่างของอัตราแลกเปลี่ยนสกุลเงินระหว่างประเทศสองกลุ่ม คุณได้สุ่มเลือกตัวอย่างของสองกลุ่ม คือกลุ่มประเทศเอเชียและกลุ่มประเทศยุโรป จงคำนวณค่าผลต่างระหว่างค่าเฉลี่ยของสองกลุ่ม และตั้งช่วงความเชื่อมั่นของผลต่างระหว่างค่าเฉลี่ยของสองกลุ่มที่รวมถึงค่า Z-score ในระดับนัยสำคัญ 0.05 และใช้วิธีการแจกแจงตัวอย่างสองกลุ่มแบบการแจกแจง t (t-distribution) ของสถิติ')
-
 print("Mean Difference:", mean_diff)
 print("t-score:", t_score)
 print("p-value:", p_value)
 print("Confidence Interval:", confidence_interval)
-
-
