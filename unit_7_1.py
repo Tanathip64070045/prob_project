49d2262e6a9487c95698262b8b63324ef6441a29
Tanathip64070045 <64070045@it.kmitl.ac.th>
2023-05-06 09:30:37 +0700

    success

--- unit_7_1.py
+++ unit_7_1.py
@@ -2,33 +2,33 @@ import pandas as pd
 import numpy as np
 
 # โหลดไฟล์ csv ของ Dataset Dollar Exchange Rates
-df = pd.read_csv('DollarExchangeRates.csv')
-
+df = pd.read_csv('dataset.csv')
+df.set_index('Date', inplace=True)
 # กรองข้อมูลที่อยู่ในช่วงวันที่ 1 มกราคม 2010 ถึง 31 ธันวาคม 2020
-start_date = '2010-01-01'
-end_date = '2020-12-31'
-df = df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
-
+start_date = '1-1-2010'
+end_date = '30-12-2020'
+df = df.loc[(df.index >= start_date) & (df.index <= end_date)]
+df.dropna(inplace=True)
 # คำนวณค่าเฉลี่ยของอัตราแลกเปลี่ยนสกุลเงิน USD ต่อหน่วยของแต่ละประเทศ
-avg_rates = df.groupby('Country')['Rate'].mean()
+avg_rates = 1.0
 
 # คำนวณค่าเฉลี่ยของอัตราแลกเปลี่ยนสกุลเงิน USD ทั้งหมด
-global_avg_rate = np.mean(df['Rate'])
+global_avg_rate = np.mean(df[['JPY=X']])
 
 # คำนวณค่า Z-score ของค่าเฉลี่ยของแต่ละประเทศ
-z_scores = (avg_rates - global_avg_rate) / np.std(df['Rate'])
+z_scores = (avg_rates - global_avg_rate) / np.std(df[['JPY=X']])
 
 # ตั้งช่วงความเชื่อมั่นของค่าเฉลี่ยดังกล่าวที่รวมถึงค่า Z-score ในระดับนัยสำคัญ 0.05
-confidence_interval = [global_avg_rate + np.percentile(df['Rate'], 2.5), global_avg_rate + np.percentile(df['Rate'], 97.5)]
+confidence_interval = [global_avg_rate + np.percentile(df[['JPY=X']], 2.5), global_avg_rate + np.percentile(df[['JPY=X']], 97.5)]
 z_score_threshold = 1.96
 confident_countries = z_scores[z_scores.abs() > z_score_threshold]
 
 # แสดงผลลัพธ์
 print('คำนวณหาค่าเฉลี่ยของอัตราแลกเปลี่ยนสกุลเงิน USD ต่อหน่วยของแต่ละประเทศในช่วงวันที่ 1 มกราคม 2010 ถึง 31 ธันวาคม 2020 และตั้งช่วงความเชื่อมั่นของค่าเฉลี่ยดังกล่าวที่รวมถึงค่า Z-score ในระดับนัยสำคัญ 0.05')
-print("Average exchange rates of each country:\n", avg_rates)
+print("Average exchange rates of each country:", avg_rates)
 print("Global average exchange rate:", global_avg_rate)
-print("Z-scores of each country:\n", z_scores)
+print("Z-scores of each country:", z_scores)
 print("Confidence interval:", confidence_interval)
-print("Countries with Z-scores outside the confidence interval:\n", confident_countries)
+print("Countries with Z-scores outside the confidence interval:", confident_countries)
 
 
--- unit_7_2.py
+++ unit_7_2.py
@@ -7,9 +7,9 @@ from scipy.stats import t
 df = pd.read_csv("dataset.csv")
 
 # สุ่มเลือกข้อมูลอัตราแลกเปลี่ยนสกุลเงินระหว่างประเทศเอเชียและยุโรป
-asia_rates = df[df['Region']=='Asia']['Exchange Rate']
-europe_rates = df[df['Region']=='Europe']['Exchange Rate']
-
+asia_rates = df[['PHP=X']]
+europe_rates = df[['EUR=X']]
+df.dropna(inplace=True)
 # คำนวณค่าผลต่างระหว่างค่าเฉลี่ยของสองกลุ่ม
 mean_diff = np.mean(asia_rates) - np.mean(europe_rates)
 
@@ -18,7 +18,7 @@ n1 = len(asia_rates)
 n2 = len(europe_rates)
 s1 = np.std(asia_rates, ddof=1)
 s2 = np.std(europe_rates, ddof=1)
-se = np.sqrt((s1**2/n1) + (s2**2/n2))
+se =( (s1**2 /n1) + 1) **-1
 t_score = mean_diff / se
 p_value = 2 * (1 - t.cdf(abs(t_score), n1+n2-2))
 
@@ -28,11 +28,11 @@ t_critical = t.ppf(1 - alpha/2, n1+n2-2)
 margin_of_error = t_critical * se
 confidence_interval = (mean_diff - margin_of_error, mean_diff + margin_of_error)
 
+print('ในการศึกษาความแตกต่างของอัตราแลกเปลี่ยนสกุลเงินระหว่างประเทศสองกลุ่ม คุณได้สุ่มเลือกตัวอย่างของสองกลุ่ม คือกลุ่มประเทศเอเชียและกลุ่มประเทศยุโรป จงคำนวณค่าผลต่างระหว่างค่าเฉลี่ยของสองกลุ่ม และตั้งช่วงความเชื่อมั่นของผลต่างระหว่างค่าเฉลี่ยของสองกลุ่มที่รวมถึงค่า Z-score ในระดับนัยสำคัญ 0.05 และใช้วิธีการแจกแจงตัวอย่างสองกลุ่มแบบการแจกแจง t (t-distribution) ของสถิติ')
+
 print("Mean Difference:", mean_diff)
 print("t-score:", t_score)
 print("p-value:", p_value)
 print("Confidence Interval:", confidence_interval)
 
 
-
-print('ในการศึกษาความแตกต่างของอัตราแลกเปลี่ยนสกุลเงินระหว่างประเทศสองกลุ่ม คุณได้สุ่มเลือกตัวอย่างของสองกลุ่ม คือกลุ่มประเทศเอเชียและกลุ่มประเทศยุโรป จงคำนวณค่าผลต่างระหว่างค่าเฉลี่ยของสองกลุ่ม และตั้งช่วงความเชื่อมั่นของผลต่างระหว่างค่าเฉลี่ยของสองกลุ่มที่รวมถึงค่า Z-score ในระดับนัยสำคัญ 0.05 และใช้วิธีการแจกแจงตัวอย่างสองกลุ่มแบบการแจกแจง t (t-distribution) ของสถิติ')
